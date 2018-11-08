3534d30c20b8e589ce6f2ffdea8c7ba513f1ed89
Felix <felix.p.hall@gmail.com>
2018-11-08 20:51:06 +1000

    deleted a double up of the same code in the UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -11,10 +11,6 @@ import inspect, os
 from django.dispatch import receiver
 
 
-#from leaders.models import GroupRecord
-
-
-
 #from login.groups import Groups
 
 # Create your models here.
@@ -56,13 +52,8 @@ class UserProfileAdminManager(models.AdminManager):
 class UserProfile(models.Model):
 
 	list_format_group = [(None, 'Click here to select the group')]
-	data_set = GroupRecord.objects.all()
-	for i in data_set:
-		group_name = i.group
-		group_abbr = i.abbreviation
-		list_format_group.append(list_maker(group_abbr, group_name))
-	tuple_format_group = tuple(list_format_group)
-	list_format_group = []
+
+#	list_format_group = []
 	data_set = GroupRecord.objects.all()
 	for i in data_set:
 		group_name = i.group
