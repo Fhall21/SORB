a6402cd6e0d773dd894c191863ec39e69dea997f
Felix <felix.p.hall@gmail.com>
2018-11-09 18:13:04 +1000

    removed dj_database_url from the else statement, for if DEBUG:

--- scouts/settings.py
+++ scouts/settings.py
@@ -59,10 +59,10 @@ else:
     # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    import dj_database_url
-    db_from_env = dj_database_url.config('postgres://postgres:Merlin99@localhost:5432/sorb')
-    DATABASES['default'].update(db_from_env)
-    DATABASES['default']['CONN_MAX_AGE'] = 500
+import dj_database_url
+db_from_env = dj_database_url.config('postgres://postgres:Merlin99@localhost:5432/sorb')
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 
